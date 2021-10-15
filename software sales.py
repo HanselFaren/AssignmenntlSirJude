c46479608f00a590e336eb17ef095d32f6e7aa96
hansel409 <hanselfaren57@gmail.com>
2021-10-15 21:03:41 +0700

    assignment

--- software sales.py
+++ software sales.py
@@ -18,11 +18,11 @@ elif 50 <= p <= 99:
     a = 99 * p * (3/10)
     total = 99 * p - a
     a1 = "${:,.2f}".format(a)
-    print(f"Discount Amount @ 20% : {a1}")
+    print(f"Discount Amount @ 30% : {a1}")
 else:
     a = 99 * p * (2/5)
     a1 = "${:,.2f}".format(a)
     total = 99 * p - a
-    print(f"Discount Amount @ 20% : {a1}")
+    print(f"Discount Amount @ 40% : {a1}")
 total1 = "${:,.2f}".format(total)
 print(f"Total Amount = {total1}")
