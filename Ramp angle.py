4191dfd80c2bdbaa03d6b1f6bbceb3bceb25b317
hansel409 <91465998+hansel409@users.noreply.github.com>
2021-10-15 21:25:05 +0700

    Delete Ramp angle.py

--- Ramp angle.py
+++
@@ -1,7 +0,0 @@
-import math
-m = float(input("Enter the mass of the cart (in kg): "))
-f = float(input("Enter the force to push the cart (in N): "))
-g = 9.8
-sin = f / (m * g)
-degrees = math.degrees(sin)
-print(f"The angle of the ramp is %0.1f degrees"%degrees)
