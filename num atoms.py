560bdaeafaf89ef28ec0f6bfd6f9a9948f58b7d1
hansel409 <91465998+hansel409@users.noreply.github.com>
2021-10-27 21:38:08 +0700

    Delete num atoms.py

--- num atoms.py
+++
@@ -1,7 +0,0 @@
-def num_atoms(grams, Aweight = 196.97):
-    atomic = grams / Aweight
-    print(atomic * 6.0222 * (10**23))
-
-x = num_atoms(10)
-y = num_atoms(10, 12.001)
-z = num_atoms(10, 1.008)
